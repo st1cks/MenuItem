4b387393643150a02e690819b488332f0bdf835e
Ricky zhao <rzhao3818@bths.edu>
2018-11-22 19:32:01 -0500

    finished

--- src/Trio.java
+++ src/Trio.java
@@ -10,9 +10,20 @@ public class Trio implements MenuItem {
     }
 
     public String getName(){
-
+    return sandwich.getName() + "/" + salad.getName() + "/" + drink.getName() + "Trio";
     }
-    public int getWeight(){
-
+    public double getPrice(){
+        double sandPrice = sandwich.getPrice();
+        double saladPrice = salad.getPrice();
+        double drinkPrice = drink.getPrice();
+       if (sandPrice > saladPrice && saladPrice > drinkPrice){
+           return sandPrice + saladPrice;
+       }
+       if (saladPrice > sandPrice && drinkPrice > sandPrice){
+           return saladPrice + drinkPrice;
+       }
+       else{
+           return sandPrice + drinkPrice;
+       }
     }
 }
